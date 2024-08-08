6bf9737edd118dd3677cc0bf5aa38c8d134bdac5
Kevin Cristensen <ke.cristensen@duocuc.cl>
2024-08-08 00:11:56 -0400

    Video Player + Permisos http

--- lib/presentation/widgets/shared/video_scrollable_view.dart
+++ lib/presentation/widgets/shared/video_scrollable_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:toktik/domain/entities/video_post.dart';
 import 'package:toktik/presentation/widgets/shared/video_buttons.dart';
+import 'package:toktik/presentation/widgets/video/fullscreen_player.dart';
 
 
 class VideoScrollableView extends StatelessWidget {
@@ -20,6 +21,14 @@ class VideoScrollableView extends StatelessWidget {
         return Stack(
           children: [
             //Video Player + Gradiente
+            SizedBox.expand(
+              child: FullScreenPlayer(
+                videoUrl: videoPost.videoUrl,
+                caption: videoPost.caption,
+              ),
+            ),
+
+
             Positioned(
               bottom: 40,
               right: 20,
